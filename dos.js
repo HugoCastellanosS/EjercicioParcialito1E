5784ed7c7ad9ecae9fcc8d123e5820645a4cac53
Hugo Castellanos <hugo.cassil@gmail.com>
2020-08-06 20:50:28 -0300

    TerminalMacOs

--- dos.js
+++ dos.js
@@ -12,5 +12,68 @@ Considerar que las categorías pueden ser: almacen, lácteos, limpieza o bebidas
 
 function mostrar()
 {
-	alert("Hola carajooo");
+	let vArticulo;
+	let vCategoría;
+	let vPrecio=0;
+	let vMayorPrecio=0;
+	let bandera=0;
+	let bandera2=0
+	let vArticuloMayorpre=0;
+	let vMayorPreLacteos=0;
+	let vNomMayorpreLacteos;
+	let vCantBebidas=0;
+	let respuesta="s";
+	
+
+	    
+	do
+	{
+		vArticulo=prompt("Ingrese el nombre de un articulo: ");
+//validacion
+		do
+		{	
+			vCategoría=prompt("Ingrese una categoria: ");
+
+		}while(!(vCategoría=="Almacen"|| vCategoría=="Lacteos"|| vCategoría=="Limpieza" || vCategoría=="Bebidas"));
+
+		do
+		{
+			vPrecio=parseInt(prompt("Ingrese un precio: "));
+
+		}while(!(vPrecio>0 && vPrecio<=1000));
+//fin validacion
+
+		if(bandera==0 || vPrecio > vMayorPrecio)
+		{
+			vMayorPrecio=vPrecio;
+			vArticuloMayorpre=vArticulo;
+			bandera=1; //la segunda vez vale 1, y la segunda vez compara
+		}
+//sa el flag para que entre por esa condición y no por la comparación
+//es solo para la primera vez que ingresa
+//porque no hay numeros para comparar
+
+		if( (bandera2==0 && vCategoría=="Lacteos") ||(vCategoría=="Lacteos" && vPrecio > vMayorPreLacteos) )
+		{
+			vMayorPreLacteos=vPrecio;
+			vNomMayorpreLacteos=vArticulo;
+			bandera2=1;
+
+		}
+
+		if(vCategoría=="Bebidas")
+		{
+			vCantBebidas++;
+		}
+
+		respuesta=prompt("desea continuar? s/n");
+
+
+	}while(respuesta=="s");
+
+
+	document.write("El articulo de mayor precio es: "+vArticuloMayorpre) + "<br>";
+	document.write("Nombre del articulo con mayor precio de los lacteos es: "+vNomMayorpreLacteos)+"<br>";
+	document.write("El numero de bebidas es: "+vCantBebidas)+"<br>";
+	
 }
